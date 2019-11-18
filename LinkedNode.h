4c71765fc463e714d4e7a02be0d4e27986789d72
sradms0 <sradms0@openmailbox.org>
2019-11-18 11:08:52 -0500

    add include for iostream and string

--- LinkedNode.h
+++ LinkedNode.h
@@ -1,16 +1,15 @@
 #ifndef LINKEDNODE_H
 #define LINKEDNODE_H
 
+#include <iostream>
+#include <string>
+
 class LinkedNode {
     public:
-        virtual LinkedNode{}
-        LinkedNode(const LinkedNode& nodeToCopy);
-        virtual ~LinkedNode(){}
-
         virtual void setItem()=0;
         virtual *LinkedNode getItem()=0;
         virtual void setNext(LinkedNode* newNext)=0;
         virtual *LinkedNode getNext()=0;
         virtual std::string toString()=0;
-    };
+};
 #endif
