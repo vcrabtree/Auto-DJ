a0c17c6ba7a5e8bbb223174fee76b62179df336c
sradms0 <sradms0@openmailbox.org>
2019-11-18 11:09:16 -0500

    add Song.h

---
+++ Song.h
@@ -0,0 +1,34 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include "LinkedNode.h"
+
+class Song: public LinkedNode {
+    private:
+        std::string artist;
+        std::string title;
+        float duration;
+        int playCount;
+
+    public:
+        Song();
+        Song(const Song& songToCopy);
+        ~Song();
+        void setItem();
+        *LinkedNode getItem();
+        void setNext(LinkedNode* newNext);
+        *LinkedNode getNext();
+        std::string toString();
+
+        std::string getArtist();
+        void setArtist(std::string artist);
+        std::string getArtist();
+        std::string setTitle(std::string title);
+        std::string getTitle();
+        void setDuration(float duration);
+        float getDuration();
+        void setPlayCount(int playCount);
+        int getPlayCount();
+}
+
+#endif
