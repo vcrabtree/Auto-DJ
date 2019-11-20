c3b7d2571c9cb302c7d393883528c27febf00cd7
sradms0 <sradms0@openmailbox.org>
2019-11-20 09:44:57 -0500

    add Song implementation

---
+++ Song.cpp
@@ -0,0 +1,33 @@
+#include "Song.h"
+
+Song::Song(std::string newTitle, std::string newArtist, float newDuration) {
+    artist = newTitle;
+    title = newArtist;
+    duration = newDuration;
+    playCount = 0;
+}
+
+void Song::setArtist(std::string newArtist) { artist = newArtist; }
+
+std::string Song::getArtist() { return artist; }
+
+std::string Song::setTitle(std::string newTitle) { title = newTitle; }
+
+std::string Song::getTitle() { return title; }
+
+void Song::setDuration(float newDuration) { duration = newDuration; }
+
+float Song::getDuration() { return duration; }
+
+void Song::setPlayCount(int newPlayCount) { playCount = newPlayCount; }
+
+int Song::getPlayCount() { return playCount; }
+
+std::string Song::toString() {
+    return 
+        "artist:\t\t"        +artist+
+        "\ntitle:\t\t"       +title+
+        "\nduration:\t"    +std::to_string(duration)+
+        "\nplay count:\t"  +std::to_string(playCount);
+}
+
