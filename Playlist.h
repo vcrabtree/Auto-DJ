f0f06f24947e6706da5a94a4d2bcbfac83a0c819
sradms0 <sradms0@openmailbox.org>
2019-11-22 22:13:43 -0500

    add title and duration properties, constructor, destructor, getDuration declartions

--- Playlist.h
+++ Playlist.h
@@ -4,18 +4,25 @@
 #include "List.h"
 #include "LinkedNode.h"
 #include "Song.h"
+#include <iostream>
 
 class Playlist: public List {
     private:
         LinkedNode<Song> *head, *tail;
+        std::string title;
+        float duration;
         int length;
 
     public:
+        Playlist(std::string title);
+        ~Playlist();
+
         void add(Song* song, int index);
         Song* remove(std::string title);
         bool isEmpty();
         void clear();
         int getLength();
+        float getDuration();
         int find(std::string searchKey);
 }
 #endif
