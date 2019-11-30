ebddf1dc9e64ce1a417e344cfd74bc2c32c220e4
Vera Crabtree <vcrabtree99@gmail.com>
2019-11-30 10:26:15 -0500

    Finished Working Version of  LibraryTest.cpp and SongTest.cpp

--- LibraryTest.cpp
+++ LibraryTest.cpp
@@ -6,11 +6,11 @@
 #include "Library.h"
 #include "Song.h"
 
-void copyConstructorTest(Library& testLibrary) {
+void copyConstructorTest(Library* testLibrary) {
     //TODO
 }
 
-void assignmentOperatorTest(Library& testLibrary) {
+void assignmentOperatorTest(Library* testLibrary) {
     //TODO
 }
 
@@ -59,48 +59,48 @@ void addAndGetSongAtTest(Library* testLibrary) {
     }
 }
 
-void removeTest(Library testLibrary) { //TODO
+void removeTest(Library* testLibrary) {
     std::cout << "\nremove Test: ";
-    testLibrary.clear();
+    testLibrary->clear();
     Song* song1 = new Song ("All I Want For Christmas Is You", "Mariah Carey", 2.9);
     Song* song2 = new Song ("A Holly Jolly Christmas", "Burl Ives", 2.27);
     Song* song3 = new Song ("Sleigh Ride", "The Ronettes", 3.03);
     Song* song4 = new Song ("Last Christmas", "Wham!", 4.6);
     Song* song5 = new Song ("Someday at Christmas", "Jackson 5", 2.64);
-    testLibrary.add(song1, 0);
-    testLibrary.add(song2, 0);
-    testLibrary.add(song3, 1);
-    testLibrary.add(song4, 2);
-    testLibrary.add(song5, 0);
-    Song* removedSong1 = testLibrary.remove(song2->getTitle(), song2->getArtist());
+    testLibrary->add(song1, 0);
+    testLibrary->add(song2, 0);
+    testLibrary->add(song3, 1);
+    testLibrary->add(song4, 2);
+    testLibrary->add(song5, 0);
+    Song* removedSong1 = testLibrary->remove(song2->getTitle(), song2->getArtist());
     if (removedSong1 == song2) {
         std::cout << "\nPassed";
     }
     else {
         std::cout << "\nFailed";
     }
-    Song* removedSong2 = testLibrary.remove(song4->getTitle(), song4->getArtist());
+    Song* removedSong2 = testLibrary->remove(song4->getTitle(), song4->getArtist());
     if (removedSong1 == song4) {
         std::cout << "\nPassed";
     }
     else {
         std::cout << "\nFailed";
     }
-    Song* removedSong3 = testLibrary.remove(song3->getTitle(), song3->getArtist());
+    Song* removedSong3 = testLibrary->remove(song3->getTitle(), song3->getArtist());
     if (removedSong1 == song3) {
         std::cout << "\nPassed";
     }
     else {
         std::cout << "\nFailed";
     }
-    Song* removedSong4 = testLibrary.remove(song1->getTitle(), song1->getArtist());
+    Song* removedSong4 = testLibrary->remove(song1->getTitle(), song1->getArtist());
     if (removedSong1 == song1) {
         std::cout << "\nPassed";
     }
     else {
         std::cout << "\nFailed";
     }
-    Song* removedSong5 = testLibrary.remove(song5->getTitle(), song5->getArtist());
+    Song* removedSong5 = testLibrary->remove(song5->getTitle(), song5->getArtist());
     if (removedSong1 == song5) {
         std::cout << "\nPassed";
     }
@@ -153,27 +153,27 @@ void getLengthTest(Library* testLibrary) {
     }
 }
 
-void findTest(Library testLibrary) {
+void findTest(Library* testLibrary) {
     std::cout << "\nfind Test: ";
     Song* song1 = new Song("What's This?", "Danny Elfman", 3.1);
     Song* song2 = new Song("Rockin' Around the Christmas Tree", "Brenda Lee", 2.1);
     Song* song3 = new Song("Hot Chocolate", "Tom Hanks", 2.52);
-    testLibrary.add(song3, 0);
-    testLibrary.add(song2, 0);
-    testLibrary.add(song1, 0);
-    if (testLibrary.find(song1->getTitle(), song1->getArtist()) == 1) {
+    testLibrary->add(song3, 0);
+    testLibrary->add(song2, 0);
+    testLibrary->add(song1, 0);
+    if (testLibrary->find(song1->getTitle(), song1->getArtist()) == 1) {
         std::cout << "\nPassed";
     }
     else {
         std::cout << "\nFailed";
     }
-    if (testLibrary.find(song2->getTitle(), song2->getArtist()) == 2) {
+    if (testLibrary->find(song2->getTitle(), song2->getArtist()) == 2) {
         std::cout << "\nPassed";
     }
     else {
         std::cout << "\nFailed";
     }
-    if (testLibrary.find(song3->getTitle(), song3->getArtist()) == 3) {
+    if (testLibrary->find(song3->getTitle(), song3->getArtist()) == 3) {
         std::cout << "\nPassed";
     }
     else {
@@ -183,17 +183,19 @@ void findTest(Library testLibrary) {
 
 void runAllLibraryTests() {
     Library* testLibrary = new Library();
-    copyConstructorTest(Library& testLibrary);
-    assignmentOperatorTest(Library& testLibrary);
-    addAndGetSongAtTest(Library testLibrary);
+    copyConstructorTest(testLibrary);
     testLibrary->clear();
-    removeTest(Library* testLibrary);
+    assignmentOperatorTest(testLibrary);
     testLibrary->clear();
-    isEmptyAndClearTest(Library* testLibrary);
+    addAndGetSongAtTest(testLibrary);
     testLibrary->clear();
-    getLengthTest(Library* testLibrary);
+    removeTest(testLibrary);
     testLibrary->clear();
-    findTest(Library testLibrary);
+    isEmptyAndClearTest(testLibrary);
+    testLibrary->clear();
+    getLengthTest(testLibrary);
+    testLibrary->clear();
+    findTest(testLibrary);
     delete testLibrary;
 }
 
--- SongTest.cpp
+++ SongTest.cpp
@@ -5,89 +5,89 @@
 #include <iostream>
 #include "Song.h"
 
-void testGetters(Song testSong) {
+void testGetters(Song* testSong) {
     std::cout << "getTitle Test: ";
-    if (testSong.getTitle() == "The Best of Both Worlds") {
+    if (testSong->getTitle() == "The Best of Both Worlds") {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.getTitle() << " instead of The Best of Both Worlds";
+        std::cout << "Failed, printed " << testSong->getTitle() << " instead of The Best of Both Worlds";
     }
     std::cout << "\ngetArtist Test: ";
-    if (testSong.getArtist() == "Hannah Montana") {
+    if (testSong->getArtist() == "Hannah Montana") {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.getArtist() << " instead of Hannah Montana";
+        std::cout << "Failed, printed " << testSong->getArtist() << " instead of Hannah Montana";
     }
     std::cout << "\ngetDuration Test: ";
-    if (testSong.getDuration() == 2.9) {
+    if (testSong->getDuration() == 2.9) {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.getDuration() << " instead of 2.9";
+        std::cout << "Failed, printed " << testSong->getDuration() << " instead of 2.9";
     }
     std::cout << "\ngetPlayCount Test: ";
-    if (testSong.getPlayCount() == 0) {
+    if (testSong->getPlayCount() == 0) {
         std::cout << "Passed";
     }
     else {
-        std::cout << "\nFailed, printed " << testSong.getDuration() << " instead of 0\n";
+        std::cout << "\nFailed, printed " << testSong->getDuration() << " instead of 0\n";
     }
     std::cout << "-----done-----";
 }
 
-void settersTest(Song testSong) {
+void settersTest(Song* testSong) {
     std::cout << "\nsetArtist Test: ";
-    testSong.setArtist("Miley Cyrus");
-    if (testSong.getArtist() == "Miley Cyrus") {
+    testSong->setArtist("Miley Cyrus");
+    if (testSong->getArtist() == "Miley Cyrus") {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.getArtist() << " instead of Miley Cyrus";
+        std::cout << "Failed, printed " << testSong->getArtist() << " instead of Miley Cyrus";
     }
     std::cout << "\nsetTitle Test: ";
-    testSong.setTitle("The Climb");
-    if (testSong.getTitle() == "The Climb") {
+    testSong->setTitle("The Climb");
+    if (testSong->getTitle() == "The Climb") {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.getTitle() << " instead of The Climb";
+        std::cout << "Failed, printed " << testSong->getTitle() << " instead of The Climb";
     }
     std::cout << "\nsetDuration Test: ";
-    testSong.setDuration(3.92);
-    if (testSong.getTitle() == 3.92) {
+    testSong->setDuration(3.92);
+    if (testSong->getDuration() == 3.92) {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.getTitle() << " instead of 3.92";
+        std::cout << "Failed, printed " << testSong->getDuration() << " instead of 3.92";
     }
     std::cout << "\nsetPlayCount Test: ";
-    testSong.setPlayCount(7);
-    if (testSong.getTitle() == 7) {
+    testSong->setPlayCount(7);
+    if (testSong->getPlayCount() == 7) {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.getTitle() << " instead of 7";
+        std::cout << "Failed, printed " << testSong->getPlayCount() << " instead of 7";
     }
     std::cout << "\n-----done-----\n";
 }
 
-void toStringTest(Song testSong) {
+void toStringTest(Song* testSong) {
     std::cout << "\ntoString Test: ";
-    if (testSong.toString() ==
+    if (testSong->toString() ==
     "artist:\t\tHannah Montana\ntitle:\t\tThe Best of Both Worlds\nduration:\t2.9\nplay count:\t0") {
         std::cout << "Passed";
     }
     else {
-        std::cout << "Failed, printed " << testSong.toString() <<
+        std::cout << "Failed, printed " << testSong->toString() <<
         " instead of artist:\t\tHannah Montana\ntitle:\t\tThe Best of Both Worlds\nduration:\t2.9\nplay count:\t0";
     }
     std::cout << "\n-----done-----";
 }
 
 void runAllSongTests() {
-    Song newSong = new Song ("The Best of Both Worlds", "Hannah Montana", 2.9);
+    Song* newSong = new Song ("The Best of Both Worlds", "Hannah Montana", 2.9);
     testGetters(newSong);
     settersTest(newSong);
     toStringTest(newSong);
