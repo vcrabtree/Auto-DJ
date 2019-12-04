aeebcc843cff038183243263f9302e770cd34a43
Vera Crabtree <vcrabtree99@gmail.com>
2019-12-03 20:37:59 -0500

    Added ArrayList.h and started turning Library.cpp and Library.h into an array list

--- Library.cpp
+++ Library.cpp
@@ -5,193 +5,77 @@
 #include "Library.h"
 
 Library::Library() {
-    front = nullptr;
-    end = nullptr;
-    int duration = 0;
+    if (initialCapacity >= 1) {
+        this->arrayList = new int[initialCapacity];
+        this->currItemCount = 0;
+        this->currCapacity = initialCapacity;
+    }
+    else {
+        throw std::invalid_argument("Library Capacity cannot be less than 1");
+    }
 }
 
 Library::~Library() {
-    while (front && front != end) {
-        LinkedNode<Song> *tempNode = front;
-        front = front->getNext();
-        delete tempNode;
-    }
-    front = nullptr;
-    end = nullptr;
+    delete[] arrayList;
 }
 
-void Library::add(Song* song, int index) {
-    if (index < 0) {
-        throw std::out_of_range("You cannot add a song at this index: " + std::to_string(index));
+void ArrayList::doubleCapacity() {
+    int newCapacity = currCapacity * 2;
+    int* newArray = new int[newCapacity];
+    for (int i = 0; i < currCapacity; i ++) {
+        newArray[i] = arrayList[i];
     }
-    if (find(song->getTitle(), song->getArtist()) == -1) {
-        if (front == nullptr) {
-            LinkedNode<Song> *newNode = new LinkedNode<Song>(*song);
-            front = newNode;
-            end = newNode;
-            newNode = nullptr;
-        }
-        int count = 0;
-        LinkedNode<Song> *currPtr = front;
-        while (currPtr != nullptr && count <= index) {
-            count += 1;
-            if (index == 0) {
-                LinkedNode<Song> *newNode = new LinkedNode<Song>(*song);
-                newNode->setNext(front);
-                front = newNode;
-                newNode = nullptr;
-            }
-            else if (currPtr->getNext() == nullptr && index == count + 1) {
-                LinkedNode<Song> *newNode = new LinkedNode<Song>(*song);
-                end->setNext(newNode);
-                end = newNode;
-                newNode = nullptr;
-            }
-            else if (count == index) {
-                LinkedNode<Song> *newNode = new LinkedNode<Song>(*song);
-                LinkedNode<Song> *tempNode = currPtr->getNext();
-                currPtr->setNext(newNode);
-                currPtr = newNode;
-                currPtr->setNext(tempNode);
-                newNode = nullptr;
-                tempNode = nullptr;
-            }
-            else {
-                currPtr = currPtr->getNext();
-            }
-        }
-        if (index > count && currPtr == nullptr) {
-            throw std::out_of_range("You cannot add a song at this index: " + std::to_string(index));
-        }
-        currPtr = nullptr;
-        duration = duration + song->getDuration();
+    delete[] arrayList;
+    arrayList = newArray;
+    currCapacity = newCapacity;
+}
+
+void Library::add(Song* song) { //Currently inserts at the end
+    if (currItemCount < currCapacity) {
+        arrayList[currItemCount] = song;
+        currItemCount++;
     }
     else {
-        throw std::invalid_argument("This song already exists in this library");
+        doubleCapacity();
+        arrayList[currItemCount] = song;
+        currItemCount++;
     }
 }
 
 Song* Library::remove(std::string title, std::string artist) {
-    if (front == nullptr) {
-        throw std::out_of_range("Library is empty");
+    if (currItemCount <= 0) {
+        throw std::out_of_range ("There are no items in this library");
     }
-    LinkedNode<Song> *currPtr = front;
-//    LinkedNode<Song> *currPtrBefore = front;
-    int count = 0;
-    Song* songToRemove;
-    if (front->getItem()->getTitle() == title) {
-        songToRemove = front->getItem();
-        LinkedNode<Song> *currPtr = front;
-        currPtr = currPtr->getNext();
-        front = currPtr;
-        currPtr = nullptr;
-        return songToRemove;
-    }
-    if (end->getItem()->getTitle() == title) {
-        int count = 0;
-        LinkedNode<Song> *currPtr = front;
-        LinkedNode<Song> *currPtrBefore = front;
-        while (currPtr->getNext() != nullptr) {
-            currPtrBefore = currPtr;
-            currPtr = currPtr->getNext();
-            count += 1;
-        }
-        if (count != 0) {
-            songToRemove = currPtr->getItem();
-            currPtrBefore->setNext(nullptr);
-        }
-        else {
-            songToRemove = currPtr->getItem();
-            front = nullptr;
-        }
-        currPtrBefore = nullptr;
-        currPtr = nullptr;
-        return songToRemove;
-    }
-    while (currPtr != nullptr) {
-        count += 1;
-        if (currPtr->getItem()->getTitle() == title && currPtr->getItem()->getArtist() == artist) {
-//            LinkedNode<Song> *beforeNodeToRemove = currPtrBefore;
-//            LinkedNode<Song> *nodeToRemove = currPtr;
-//            LinkedNode<Song> *afterNodeToRemove  = currPtr->getNext();
-            LinkedNode<Song> *beforeNodeToRemove = currPtr;
-            LinkedNode<Song> *nodeToRemove = beforeNodeToRemove->getNext();
-            LinkedNode<Song> *afterNodeToRemove  = nodeToRemove->getNext();
-            beforeNodeToRemove->setNext(afterNodeToRemove);
-            songToRemove = nodeToRemove->getItem();
-            delete nodeToRemove;
-            beforeNodeToRemove = nullptr;
-            afterNodeToRemove = nullptr;
-            currPtr = nullptr;
-            //currPtrBefore = nullptr;
-            duration = duration - songToRemove->getDuration();
-            return songToRemove;
-        }
-        else {
-            currPtr = currPtr->getNext();
-//            if (currPtr == front) {
-//                currPtr = currPtr->getNext();
-//            }
-//            else {
-//                currPtr = currPtr->getNext();
-//                currPtrBefore = currPtr->getNext();
-//            }
-        }
-    }
-    currPtr = nullptr;
-    throw std::invalid_argument("Song Not in List");
+    //TODO
 }
 
 Song* Library::getSongAt(int index) {
-    if (index < 0 || front == nullptr) {
-        throw std::out_of_range("Invalid Index");
+    if (index < 0) {
+        throw std::out_of_range ("There is no song at this index");
     }
-    int count = 0;
-    LinkedNode<Song> *currPtr = front;
-    while (count != index) {
-        if (currPtr->getNext() != nullptr) {
-            currPtr = currPtr->getNext();
-            count += 1;
-        }
-        else {
-            throw std::out_of_range("Invalid Index");
-        }
+    else if (index < currItemCount) {
+        return arrayList[index];
     }
-    Song* songToGet = currPtr->getItem();
-    currPtr = nullptr;
-    return songToGet;
+    else {
+        throw std::out_of_range ("There is no song at this index");
+    }
 }
 
 bool Library::isEmpty() {
-    if (front == nullptr && end == nullptr) {
+    if (currItemCount == 0) {
         return true;
     }
-    return false;
+    else {
+        return false;
+    }
 }
 
 void Library::clear() {
-    while (front && front != end) {
-        LinkedNode<Song> *tempNode = front;
-        front = front->getNext();
-        delete tempNode;
-    }
-    duration = 0;
-    front = nullptr;
-    end = nullptr;
+    currItemCount = 0;
 }
 
 int Library::getLength() {
-    if (front == nullptr) {
-        return 0;
-    }
-    int count = 0;
-    LinkedNode<Song> *currPtr = front;
-    while (currPtr->getNext() != nullptr) {
-        currPtr = currPtr->getNext();
-        count += 1;
-    }
-    currPtr = nullptr;
-    return count;
+    return currItemCount;
 }
 
 float Library::getDuration() {
@@ -199,22 +83,15 @@ float Library::getDuration() {
 }
 
 int Library::find(std::string title, std::string artist) {
-    if (front == nullptr) {
-        return -1;
-    }
-    int count = 0;
-    LinkedNode<Song> *currPtr = front;
-    while (currPtr != nullptr) {
-        if (currPtr->getItem()->getTitle() == title && currPtr->getItem()->getArtist() == artist) {
-            currPtr = nullptr;
-            return count;
-        }
-        else {
-            currPtr = currPtr->getNext();
-            count += 1;
+    for (int i = 0; i < currItemCount; i++) {
+        if (arrayList[i]->getTitle == title && arrayList[i]->getArtist == artist) {
+            return i;
         }
     }
-    currPtr = nullptr;
     return -1;
+}
+
+std::string Library::toString() {
+    //TODO
 }
 
