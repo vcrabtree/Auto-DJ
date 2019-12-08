c8e55c11a85859e60a512bd46e2a622cdf5de8ca
sradms0 <sradms0@openmailbox.org>
2019-12-08 09:06:09 -0500

    move helper functions to TestLib

--- PlaylistTest.cpp
+++ PlaylistTest.cpp
@@ -3,36 +3,6 @@
 #include "Playlist.h"
 #include "TestLib.h"
 
-Library* generateLibrary(int size, int minSongMinute, int maxSongMinute) {
-    Library *library = new Library(size);
-    std::string title, artist;
-
-    float minute = (float)minSongMinute, seconds = 0.0f, currDuration = 0.0f;
-    for (int i = 0, s = 1, m = minSongMinute; i < size; i++, m++, s++) {
-        if (currDuration >= maxSongMinute) {
-            minute = minSongMinute;
-            m = s = 0;
-        } 
-        seconds = (float)(s*s)/10;
-        currDuration = minute+seconds;
-
-        title = "title "+std::to_string(i);
-        artist = "artist "+std::to_string(i);
-        library->add(new Song(title, artist, currDuration));
-    }
-    return library;
-}
-
-Playlist* generateEmptyPlaylist(std::string title="") {
-    title = title.length() ? title : "playlist";
-    return new Playlist(title);
-}
-
-void fillPlaylist(Library *library, Playlist *emptyPlaylist) {
-    for (int i = 0; i < library->getLength(); i++)
-        emptyPlaylist->add( library->getSongAt(i) );
-}
-
 void getLengthAndDurationTest(Library *library, Playlist *filledPlaylist) {
     printTestHeader("getLengthAndDurationTest");
 
--- TestLib.h
+++ TestLib.h
@@ -16,6 +16,12 @@ void printExceptionFail(std::string name);
 template <typename T>
 void printAssert(T value, T expected);
 
+Library* generateLibrary(int size, int minSongMinute, int maxSongMinute);
+
+Playlist* generateEmptyPlaylist(std::string title="");
+
+void fillPlaylist(Library *library, Playlist *emptyPlaylist);
+
 #include "TestLib.inl"
 
 #endif
