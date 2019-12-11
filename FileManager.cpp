4410914c7f2da926a0c96fd25574e62ce257dfe1
sradms0 <sradms0@openmailbox.org>
2019-12-11 13:06:44 -0500

    change return type of readFromFile to string, and add appendToFile

--- FileManager.cpp
+++ FileManager.cpp
@@ -5,16 +5,18 @@
 #include <iostream>
 #include "FileManager.h"
 
-void FileManager::readFromFile(std::string fileName){
+std::string FileManager::readFromFile(std::string fileName){
     std::ifstream infile(fileName);
+    std::string stringToWrite = "";
     if (infile) {
         while (infile) {
-            std::string stringToCheck;
-            getline(infile, stringToCheck);
-            std::cout << stringToCheck << std::endl;
-            }
+            std::string stringToRead;
+            getline(infile, stringToRead);
+            stringToWrite += stringToRead+"\n";
         }
-    else{
+        infile.close();
+        return stringToWrite;
+    } else {
         std::cout << "File not found" << std::endl;
     }
 }
@@ -30,3 +32,14 @@ void FileManager::writeToFile(std::string fileName, std::string stringToWrite){
     }
 }
 
+void FileManager::appendToFile(std::string fileName, std::string stringToWrite){
+    std::ofstream outf;
+    outf.open(fileName, std::ios_base::app);
+    if (outf){
+        outf << stringToWrite;
+        outf.close();
+    }
+    else{
+        std::cerr << "Can't write to file" << std::endl;
+    }
+}
